73ef4daeee9a1d26d3b217e7ea45de8da0410300
FsxShader2012 <robin@moonlightgamestg.de>
2015-11-01 18:10:02 +0100

    Added Rainbow armor and rainbow shields

--- rainbow_ore/README.md
+++ rainbow_ore/README.md
@@ -1,10 +1,8 @@
-===========
 Rainbow Ore
 ===========
 
 This mod features a new ore called "Rainbow Ore" (as if you guessed it :D) wich is pretty rare but also pretty powerful.
 
-=======
 License
 =======
 
@@ -14,10 +12,13 @@ Rainbow Ore code is licensed under the GNU LGPLv2.1.
 
 Textures are licensed under the same license as minetest_game.
 
-=========
 Crafting:
 =========
 
 Smelt "Rainbow Ore Block" --> "Rainbow Ingots"
 
 Tools are crafted as always but with "Rainbow Ingots" as material instead.
+
+Armor is crafted like Armor but with "Rainbow Ingots" as material instead.
+
+Shield are crafted like a shield but with "Rainbow Ingots" as material instead.
--- rainbow_ore/init.lua
+++ rainbow_ore/init.lua
@@ -151,3 +151,84 @@ minetest.register_ore({
 	height_min = -31000,
 	height_max = -100,
 })
+
+
+--Define Rainbow Armor
+minetest.register_tool("rainbow_ore:rainbow_ore_helmet", {
+	description = "Rainbow Helmet",
+	inventory_image = "rainbow_ore_helmet_inv.png",
+	groups = {armor_head=30, armor_heal=24, armor_use=50},
+	wear = 0,
+})
+minetest.register_tool("rainbow_ore:rainbow_ore_chestplate", {
+	description = "Rainbow Chestplate",
+	inventory_image = "rainbow_ore_chestplate_inv.png",
+	groups = {armor_torso=40, armor_heal=24, armor_use=50},
+	wear = 0,
+})
+minetest.register_tool("rainbow_ore:rainbow_ore_leggings", {
+	description = "Rainbow Leggings",
+	inventory_image = "rainbow_ore_leggings_inv.png",
+	groups = {armor_legs=40, armor_heal=24, armor_use=50},
+	wear = 0,
+})
+minetest.register_tool("rainbow_ore:rainbow_ore_boots", {
+	description = "Rainbow Boots",
+	inventory_image = "rainbow_ore_boots_inv.png",
+	groups = {armor_feet=30, armor_heal=24, armor_use=50},
+	wear = 0,
+})
+
+
+--Define Rainbow shield
+minetest.register_tool("rainbow_ore:rainbow_ore_shield", {
+	description = "Rainbow Shield",
+	inventory_image = "rainbow_ore_shield_inv.png",
+	groups = {armor_shield=30, armor_heal=24, armor_use=50},
+	wear = 0,
+	})
+
+
+--Define Rainbow Armor crafting recipe
+minetest.register_craft({
+	output = "rainbow_ore:rainbow_ore_helmet",
+	recipe = {
+		{"rainbow_ore:rainbow_ore_ingot", "rainbow_ore:rainbow_ore_ingot", "rainbow_ore:rainbow_ore_ingot"},
+		{"rainbow_ore:rainbow_ore_ingot", "", "rainbow_ore:rainbow_ore_ingot"},
+		{"", "", ""},
+	},
+})
+minetest.register_craft({
+	output = "rainbow_ore:rainbow_ore_chestplate",
+	recipe = {
+		{"rainbow_ore:rainbow_ore_ingot", "", "rainbow_ore:rainbow_ore_ingot"},
+		{"rainbow_ore:rainbow_ore_ingot", "rainbow_ore:rainbow_ore_ingot", "rainbow_ore:rainbow_ore_ingot"},
+		{"rainbow_ore:rainbow_ore_ingot", "rainbow_ore:rainbow_ore_ingot", "rainbow_ore:rainbow_ore_ingot"},
+	},
+})
+minetest.register_craft({
+	output = "rainbow_ore:rainbow_ore_leggings",
+	recipe = {
+		{"rainbow_ore:rainbow_ore_ingot", "rainbow_ore:rainbow_ore_ingot", "rainbow_ore:rainbow_ore_ingot"},
+		{"rainbow_ore:rainbow_ore_ingot", "", "rainbow_ore:rainbow_ore_ingot"},
+		{"rainbow_ore:rainbow_ore_ingot", "", "rainbow_ore:rainbow_ore_ingot"},
+	},
+})
+minetest.register_craft({
+	output = "rainbow_ore:rainbow_ore_boots",
+	recipe = {
+		{"rainbow_ore:rainbow_ore_ingot", "", "rainbow_ore:rainbow_ore_ingot"},
+		{"rainbow_ore:rainbow_ore_ingot", "", "rainbow_ore:rainbow_ore_ingot"},
+	},
+})
+
+
+--Define Rainbow shield crafting recipe
+minetest.register_craft({
+	output = "rainbow_ore:rainbow_ore_shield",
+	recipe = {
+		{"rainbow_ore:rainbow_ore_ingot", "rainbow_ore:rainbow_ore_ingot", "rainbow_ore:rainbow_ore_ingot"},
+		{"rainbow_ore:rainbow_ore_ingot", "rainbow_ore:rainbow_ore_ingot", "rainbow_ore:rainbow_ore_ingot"},
+		{"", "rainbow_ore:rainbow_ore_ingot", ""},
+	},
+})
